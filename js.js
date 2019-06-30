cf6a3f021036eeaad09a32881f14f7e6ec3a656e
cuchuoibothay <cuchuoibothay@gmail.com>
2019-07-01 04:40:51 +0700

    su ly hiey ung jquery

---
+++ js.js
@@ -0,0 +1,21 @@
+ $(function(){
+ 	 	 
+ 	 $(".xuong,.kn").click(function(){
+ 	  	$('body,html').animate({scrollTop:$('#khoiedu').offset().top},700);
+ 	  	return false ; 
+ 	  
+ 	 })
+ 	$(".sp").click(function(){
+ 	  	$('body,html').animate({scrollTop:$('#khoisanpham').offset().top},700);
+ 	  	return false ; 
+ 	  
+ 	 })
+ 	
+	// $(".cm").click(function(){
+ // 	  	$('body,html').animate({scrollTop:$('#phanhoi').offset().top},700);
+ // 	  	return false ; 
+ 	  
+ // 	 })
+
+})  
+ 
