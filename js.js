f74c0e0c1c8770553c1de4502d0dc1c3429a3eaa
cuchuoibothay <cuchuoibothay@gmail.com>
2019-07-02 17:26:07 +0700

    su ly comment fb

--- js.js
+++ js.js
@@ -11,11 +11,11 @@
  	  
  	 })
  	
-	// $(".cm").click(function(){
- // 	  	$('body,html').animate({scrollTop:$('#phanhoi').offset().top},700);
- // 	  	return false ; 
+	$(".cm").click(function(){
+ 	  	$('body,html').animate({scrollTop:$('#phanhoi').offset().top},700);
+ 	  	return false ; 
  	  
- // 	 })
+ 	 })
 
 })  
  
